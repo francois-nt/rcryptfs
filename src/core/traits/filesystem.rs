2c732823958df35afa7792cf51ff7e2a30d846cf
François <63752597+francois-nt@users.noreply.github.com>
2026-08-21 14:13:54 +0200

    refactor traits.rs in traits subfolder

--- src/core/traits/filesystem.rs
+++ src/core/traits/filesystem.rs
@@ -1,19 +1,10 @@
-use super::{
+use super::super::{
     FileType, FsDirEntry, GenericOpenOptions, Metadata, Permissions, Result, UnsafeCache,
-    VirtualPath, VirtualPathBuf,
+    VirtualPath,
 };
 pub use camino::{Utf8Path, Utf8PathBuf};
 use log::error;
 use std::{fmt::Display, time::SystemTime};
-
-mod helpers;
-pub(crate) use helpers::temp_file_path;
-use helpers::{
-    default_create_symlink, default_metadata, default_mkdir, default_mknode, default_read_symlink,
-    default_remove, default_remove_dir, default_rename, default_set_permissions, default_set_time,
-};
-mod crypto_backend;
-pub use crypto_backend::*;
 
 /// Provides positioned reads without changing a shared file cursor.
 pub trait ReadAt {
---
+++ src/core/traits/mod.rs
@@ -0,0 +1,16 @@
+use super::{
+    FileType, FsDirEntry, GenericOpenOptions, Metadata, Permissions, Result, VirtualPath,
+    VirtualPathBuf,
+};
+
+mod crypto_backend;
+mod filesystem;
+mod helpers;
+
+pub use crypto_backend::*;
+pub use filesystem::*;
+pub(crate) use helpers::temp_file_path;
+use helpers::{
+    default_create_symlink, default_metadata, default_mkdir, default_mknode, default_read_symlink,
+    default_remove, default_remove_dir, default_rename, default_set_permissions, default_set_time,
+};
